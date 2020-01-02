c0262fdb33e30a77566ba52f799f706af5702ce1
= <nncl@live.com>
2020-01-02 20:01:13 -0300

    Testing form

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 
 export default function TechList() {
     const [techs, setTechs] = useState([]);
+    const [newTech, setNewTech] = useState('');
 
     function handleAddTech() {
-        setTechs([...techs, 'Node.js']);
+        setTechs([...techs, newTech]);
+        setNewTech('');
     }
 
-    return <div>
-        <ul data-testid="tech-list">
-            {techs.map(tech => <li key={tech}>{tech}</li>)}
-        </ul>
+    return (
+        <form data-testid="tech-form" onSubmit={handleAddTech}>
+            <ul data-testid="tech-list">
+                {techs.map(tech => <li key={tech}>{tech}</li>)}
+            </ul>
 
-        <button onClick={handleAddTech}>Add</button>
-    </div>
+            <label htmlFor="tech">Tech</label>
+            <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+
+            <button>Add</button>
+        </form>
+    );
 }
--- src/components/TechList/index.test.js
+++ src/components/TechList/index.test.js
@@ -4,10 +4,18 @@ import TechList from "~/components/TechList/index";
 
 describe('TechListComponent', () => {
     it('should be able to add new tech', () => {
-        const { getByText, getByTestId } = render(<TechList />);
+        const { getByText, getByTestId, getByLabelText } = render(<TechList />);
 
-        fireEvent.click(getByText('Add'));
+        // Fill input with some value
+        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
 
+        // Submit form
+        fireEvent.submit(getByTestId('tech-form'));
+
+        // Verify if item has been added on the list
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+
+        // Verify if input has been reseted
+        expect(getByLabelText('Tech')).toHaveValue('');
     });
 });
