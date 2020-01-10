dc315dbc63851637d793395b90fc1b069d71e959
= <nncl@live.com>
2020-01-10 16:40:05 -0300

    - Adding comment - Refactoring used value

--- src/components/TechList/index.test.js
+++ src/components/TechList/index.test.js
@@ -4,16 +4,18 @@ import TechList from "~/components/TechList/index";
 
 describe('TechListComponent', () => {
     it('should be able to add new tech', () => {
+        // Render component
         const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+        const value = 'Node.js';
 
         // Fill input with some value
-        fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+        fireEvent.change(getByLabelText('Tech'), { target: { value: value } });
 
         // Submit form
         fireEvent.submit(getByTestId('tech-form'));
 
         // Verify if item has been added on the list
-        expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+        expect(getByTestId('tech-list')).toContainElement(getByText(value));
 
         // Verify if input has been reseted
         expect(getByLabelText('Tech')).toHaveValue('');
