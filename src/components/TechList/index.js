58e0a7453f48a8e46fbb4a6c4ea7dc7888255414
= <nncl@live.com>
2020-01-10 18:10:00 -0300

    Testing and mocking useDispatch

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { addTech } from '../../store/modules/techs/actions';
+
 export default function TechList() {
     const [newTech, setNewTech] = useState('');
     const techs = useSelector(state => state.techs);
     const dispatch = useDispatch();
 
     function handleAddTech() {
-        dispatch({ type: 'ADD_TECH', payload: { tech: newTech } });
+        dispatch(addTech(newTech));
         setNewTech('');
     }
 
--- src/components/TechList/index.test.js
+++ src/components/TechList/index.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
 
 import TechList from "~/components/TechList/index";
+import { addTech } from '~/store/modules/techs/actions';
 
 jest.mock('react-redux');
 
@@ -19,5 +20,20 @@ describe('TechListComponent', () => {
         // Verify if list contains values
         expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
         expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+    });
+
+    it('should be able to add new tech', () => {
+        const { getByTestId, getByLabelText } = render(<TechList />)
+        const dispatch = jest.fn();
+        const value = 'Node.js';
+
+        useDispatch.mockReturnValue(dispatch);
+
+        // Fill input and submit its form
+        fireEvent.change(getByLabelText('Tech'), { target: { value: value } });
+        fireEvent.submit(getByTestId('tech-form'));
+
+        // Verify if dispatch has been fired with given params
+        expect(dispatch).toHaveBeenCalledWith(addTech(value));
     });
 });
